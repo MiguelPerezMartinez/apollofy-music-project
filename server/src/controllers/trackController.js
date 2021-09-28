1ff596af4158423a5e8936c4e94ef3a1f0ecbda0
Marc <marcsr03@gmail.com>
2021-09-28 20:27:07 +0200

    #69 updateTrack, deleteTrack --> done,

--- server/src/controllers/trackController.js
+++ server/src/controllers/trackController.js
@@ -1,6 +1,20 @@
 const { Tracks } = require("../models");
 
-async function getById(req, res) {
+async function getAllTracks(req, res) {
+  try {
+    const tracks = await Tracks.find({});
+    return res.status(200).send({
+      tracks: tracks,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send({
+      error: error.message,
+    });
+  }
+}
+
+async function getTrackById(req, res) {
   const { id } = req.params;
   try {
     const foundTrack = await Tracks.findOne({
@@ -53,8 +67,35 @@ async function deleteTrack(req, res) {
   }
 }
 
+async function updateTrack(req, res) {
+  const { id } = req.params;
+  try {
+    const dbResponse = await Tracks.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!dbResponse) {
+      return res.status(400).send({
+        data: null,
+        error: "Track ID doesn't exist",
+      });
+    } else {
+      res.status(200).send({
+        message: "Track updated successfully",
+        updatedTrack: dbResponse,
+      });
+    }
+  } catch (error) {
+    res.status(500).send({
+      data: req.params.id,
+      error: error.message,
+    });
+  }
+}
+
 module.exports = {
-  getById: getById,
+  getAllTracks: getAllTracks,
+  getTrackById: getTrackById,
   uploadTrack: uploadTrack,
   deleteTrack: deleteTrack,
+  updateTrack: updateTrack,
 };
--- server/src/routes/trackRoutes.js
+++ server/src/routes/trackRoutes.js
@@ -4,8 +4,10 @@ const { trackController } = require("../controllers");
 const { authMiddleware } = require("../middlewares");
 
 //end points routes:
-trackRouter.get("/get-track/:id", trackController.getById);
+trackRouter.get("/", trackController.getAllTracks);
+trackRouter.get("/get-track/:id", trackController.getTrackById);
 trackRouter.post("/upload-track", trackController.uploadTrack);
 trackRouter.delete("/delete-track/:id", trackController.deleteTrack);
+trackRouter.patch("/update-track/:id", trackController.updateTrack);
 
 module.exports = trackRouter;
