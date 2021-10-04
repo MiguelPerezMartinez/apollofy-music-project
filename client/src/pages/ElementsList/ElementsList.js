067bd220f7d5c368a270cf4b4b2f7d44b5343f8a
Jonathan Cedeño <jony.cede@gmail.com>
2021-10-04 14:48:38 +0200

    ElementList render with the selected tracks

--- client/src/pages/ElementsList/ElementsList.js
+++ client/src/pages/ElementsList/ElementsList.js
@@ -4,6 +4,12 @@ import { useHistory } from "react-router";
 
 import withAuth from "../../hoc/withAuth";
 import BarsAndModal from "../../hoc/BarsAndModal";
+import Track from "../../components/Track";
+
+import {
+  getMyTracksByUserId,
+  getFavouriteTracksByUserId,
+} from "../../services/api/apiAuth";
 
 // List types
 const MY_TRACKS = "/my-tracks";
@@ -15,19 +21,39 @@ function ElementsList() {
   const { pathname } = location;
 
   // To check the userId
-  const userData = useSelector((state) => state.userReducer);
-  console.log(userData);
+  const { data } = useSelector((state) => state.userReducer);
+  const { userId } = data;
+
   const [listType, setListType] = useState("");
+  const [dataToRender, setDataToRender] = useState([]);
+
+  async function fetchTracks() {
+    if (pathname === MY_TRACKS) {
+      const { data } = await getMyTracksByUserId(userId);
+      setDataToRender(data.data);
+    } else if (pathname === FAVOURITE_TRACKS) {
+      const { data } = await getFavouriteTracksByUserId(userId);
+      setDataToRender(data.data);
+    }
+  }
 
   useEffect(() => {
-    if (pathname === MY_TRACKS) setListType(MY_TRACKS);
-    else if (pathname === FAVOURITE_TRACKS) setListType(FAVOURITE_TRACKS);
-  }, [pathname]);
+    if (pathname === MY_TRACKS) {
+      fetchTracks();
+      setListType(MY_TRACKS);
+    } else if (pathname === FAVOURITE_TRACKS) {
+      fetchTracks();
+      setListType(FAVOURITE_TRACKS);
+    }
+  }, []);
 
   return (
     <main className="">
       {listType === MY_TRACKS && <h1>My tracks</h1>}
       {listType === FAVOURITE_TRACKS && <h1>Favourite tracks</h1>}
+      {dataToRender.length === 0 && <div>There aren't tracks</div>}
+      {dataToRender.length > 0 &&
+        dataToRender.map((track) => <Track dataTrack={track} />)}
     </main>
   );
 }
