7562af329c7a2069929e96720880b625916e615d
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2021-10-04 10:19:51 +0200

    #83 create audio spectrum --> in progress

--- client/src/components/WaveSound/WaveSound.js
+++ client/src/components/WaveSound/WaveSound.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { isPlay, isPlayBarDisplayed } from "../../redux/trackData/actions";
 import { setWaveSurfer } from "../../redux/trackData/actions";
 import WaveSurfer from "wavesurfer.js";
+import AudioSpectrum from "react-audio-spectrum";
 // import sound from "./sound2.wav";
 function WaveSound({ trackUrl }) {
   const dispatch = useDispatch();
@@ -43,6 +44,28 @@ function WaveSound({ trackUrl }) {
   }
   return (
     <>
+      <audio
+        id="audio-element"
+        src={trackUrl}
+        crossorigin="anonymous"
+        autoPlay
+      />
+      <AudioSpectrum
+        id={"audio-canvas"}
+        height={200}
+        width={300}
+        audioId={"audio-element"}
+        capColor={"red"}
+        capHeight={2}
+        meterWidth={20}
+        meterCount={512}
+        meterColor={[
+          { stop: 0, color: "#f00" },
+          { stop: 0.5, color: "#0CD7FD" },
+          { stop: 1, color: "red" },
+        ]}
+        gap={4}
+      />
       <div className="wave" ref={waveformRef}></div>
     </>
   );
